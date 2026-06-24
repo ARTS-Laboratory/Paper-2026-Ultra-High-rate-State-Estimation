80ce2755d2b3b7de212739946a6e23e859da8e4a
Austin Downey <austindowney@gmail.com>
2026-06-24 11:40:44 -0400

    AD-3
    A cleaned up version with index array, but perfect, but closer to what is needed. Could now scale to lager arrays.

---
+++ FPGA Implementation/Austin-workspace/V0.4.7-AD-3/simple_math.m
@@ -0,0 +1,16 @@
+clear
+close all 
+clc
+
+
+A = [1 2;
+     3 4];        % 2x2 weight matrix
+
+B = [1;
+     2];          % 2x1 input vector
+
+
+C = A * B;        % 2x1 output vector
+
+
+disp(C)
